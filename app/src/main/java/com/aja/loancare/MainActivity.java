953b840a961416cc35e76bbb0a6f6e07c3b945ed
asus <adarshms00.am@gmail.com>
2020-09-14 16:57:43 +0530

    tempn

--- app/src/main/java/com/aja/loancare/MainActivity.java
+++ app/src/main/java/com/aja/loancare/MainActivity.java
@@ -1,32 +1,55 @@
 package com.aja.loancare;
-import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.List;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    String[] banks = {"SBI", "HDFC", "PNB", "DXB"};
-
+class AndroidSpinnerExampleActivity extends Activity implements OnItemSelectedListener{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Spinner spin = (Spinner) findViewById(R.id.select_bank);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, banks);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spin.setAdapter(adapter);
-        spin.setOnItemSelectedListener(this);
+
+        // Spinner element
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> banks = new ArrayList<String>();
+        banks.add("SBI");
+        banks.add("HDFC");
+        banks.add("PNB");
+        banks.add("Axis");
+        banks.add("Canara");
+        banks.add("KGB");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, banks);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
     }
 
     @Override
-    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
-        Toast.makeText(getApplicationContext(), "Selected bank: " + banks[position], Toast.LENGTH_SHORT).show();
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
     }
-    @Override
     public void onNothingSelected(AdapterView<?> arg0) {
-        // TODO - Custom Code
+        // TODO Auto-generated method stub
     }
 }

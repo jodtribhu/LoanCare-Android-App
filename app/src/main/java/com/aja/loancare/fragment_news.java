9c1f42825c9f3289def74e41e26345487e99a40c
Jodiss Tribhu <jodiss.tri@gmail.com>
2020-09-21 17:54:53 +0530

    Commit 16:Emi calculator countries selection and its currency

--- app/src/main/java/com/aja/loancare/fragment_news.java
+++ app/src/main/java/com/aja/loancare/fragment_news.java
@@ -11,23 +11,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class fragment_news extends Fragment {
-    Button settings;
+    ImageButton settings;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.fragment_news, container, false);
-        Intent intent=new Intent(getActivity(),SettingsActivity.class);
-        startActivity(intent);
         settings=v.findViewById(R.id.settings_id);
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences sharedPreferences=PreferenceManager.getDefaultSharedPreferences(getActivity());
-                    String Country=String.valueOf(sharedPreferences.getString("Countries","India"));
-                Toast.makeText(getActivity(), "The country is "+Country, Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(getActivity(),SettingsActivity.class);
+                startActivity(intent);
+
             }
         });
         return v;

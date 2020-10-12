69153ccb455902b91f14527a1ed77a77fb7e6efa
asus <adarshms00.am@gmail.com>
2020-10-12 20:29:08 +0530

    loan form

--- app/src/main/java/com/aja/loancare/LoanForm.java
+++ app/src/main/java/com/aja/loancare/LoanForm.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,17 +17,20 @@ import android.widget.Spinner;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemSelectedListener;
 
-public class LoanForm extends Activity implements DatePickerDialog.OnDateSetListener, View.OnClickListener {
+public class LoanForm extends Activity implements View.OnClickListener,DatePickerDialog.OnDateSetListener,OnItemSelectedListener {
     EditText txtDate;
+    Button b1;
     private int mYear, mMonth, mDay;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loan_form);
+
         // Spinner element
-        /*
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        // Spinner Drop down elements
+
+        Spinner s1 = (Spinner) findViewById(R.id.spinnerLoan);
+        s1.setOnItemSelectedListener(this);
+       /* // Spinner Drop down elements
         List<String> banks = new ArrayList<String>();
         banks.add("-");
         banks.add("SBI");
@@ -50,8 +54,17 @@ public class LoanForm extends Activity implements DatePickerDialog.OnDateSetList
         */
 
         txtDate=(EditText)findViewById(R.id.in_date);
+        txtDate.setOnClickListener(this);
     }
 
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String selected=parent.getItemAtPosition(position).toString();
+        if(selected == "others"){
+
+        }
+    }
 
     public void onNothingSelected(AdapterView<?> arg0) {
         // TODO Auto-generated method stub
@@ -59,23 +72,25 @@ public class LoanForm extends Activity implements DatePickerDialog.OnDateSetList
 
     @Override
     public void onClick(View v) {
-        if(v == txtDate){
-        final Calendar c = Calendar.getInstance();
-        mYear = c.get(Calendar.YEAR);
-        mMonth = c.get(Calendar.MONTH);
-        mDay = c.get(Calendar.DAY_OF_MONTH);
+        if (v == txtDate) {
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
 
 
-        DatePickerDialog datePickerDialog = new DatePickerDialog
-                (this, this, mYear, mMonth, mDay);
-        datePickerDialog.show();
-    }}
+            DatePickerDialog datePickerDialog = new DatePickerDialog
+                    (this, this, mYear, mMonth, mDay);
+            datePickerDialog.show();
+
+        }
+    }
 
 
     @Override
     public void onDateSet(DatePicker datePicker, int dayOfMonth, int monthOfYear, int year) {
         txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1)
                 + "-" + year);
-    }
 
+    }
 }
